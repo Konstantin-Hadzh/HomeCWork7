33e76befff926308204e470639a534cba4ab87c7
VGEEK BRANCH <vgeekbranch@MacBook-Pro-VGEEK.local>
2022-10-26 12:52:11 +0300

    task52

---
+++ task52/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее 
+арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
+*/
+
+Console.Write("введите количество строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите количество столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] numbers = new int[n, m];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine();
+
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        avarage = (avarage + numbers[i, j]);
+    }
+    avarage = avarage / n;
+    Console.Write(avarage + "; ");
+}
+Console.WriteLine();
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void printColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+void PrintArray(int[,] array)
+{
+    Console.WriteLine();
+    Console.Write("\t");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        printColorData(j+"\t");
+    }
+    Console.WriteLine();
+    for (int j = 0; j < array.GetLength(0); j++)
+    {
+        printColorData(j+"\t");
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+            Console.Write(array[j, i] + "\t");
+        }
+        Console.WriteLine ("\t");
+    }
+}
+Console.WriteLine();
